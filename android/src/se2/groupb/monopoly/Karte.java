f818ab4266cce70227dad28b9ef557079517ca27
vivi0815 <vihaferkamp@edu.aau.at>
2022-04-03 18:14:39 +0200

    Karte Klasse

---
+++ android/src/se2/groupb/monopoly/Karte.java
@@ -0,0 +1,27 @@
+package se2.groupb.monopoly;
+
+public class Karte {
+    int id;
+    String name;
+
+    public Karte(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
