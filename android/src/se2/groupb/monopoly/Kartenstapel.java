3a6fa9dc7f0bd80409f610b1aede0a188855fb3d
vivi0815 <vihaferkamp@edu.aau.at>
2022-04-03 19:24:03 +0200

    zieheKarte Methode -> Nur Aufbau - noch nicht fertig programmiert

--- android/src/se2/groupb/monopoly/Kartenstapel.java
+++ android/src/se2/groupb/monopoly/Kartenstapel.java
@@ -5,15 +5,106 @@ import java.util.Collections;
 
 public class Kartenstapel {
     ArrayList<Karte> kartenStapel;
+    int index;
 
     public Kartenstapel(){
         kartenStapel=new ArrayList<>();
+        index=0;
     }
+
     public void add(Karte k){
         kartenStapel.add(k);
     }
+
     public void shuffle(){
         Collections.shuffle(kartenStapel);
+    }
+
+    public void zieheKarte(){
+        Karte k=kartenStapel.get(index);
+
+        if(index<kartenStapel.size()-1){
+            index++;
+        }
+         else{
+             index=0;
+        }
+        switch(k.getId()){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                break;
+            case 13:
+                break;
+            case 14:
+                break;
+            case 15:
+                break;
+            case 16:
+                break;
+            case 17:
+                break;
+            case 18:
+                break;
+            case 19:
+                break;
+            case 20:
+                break;
+            case 21:
+                break;
+            case 22:
+                break;
+            case 23:
+                break;
+            case 24:
+                break;
+            case 25:
+                break;
+            case 26:
+                break;
+            case 27:
+                break;
+            case 28:
+                break;
+            case 29:
+                break;
+            case 30:
+                break;
+            case 31:
+                break;
+            case 32:
+                break;
+            case 33:
+                break;
+            case 34:
+                break;
+            case 35:
+                break;
+            case 36:
+                break;
+            default:
+                break;
+        }
     }
 
     public static void main(String[] args) {
