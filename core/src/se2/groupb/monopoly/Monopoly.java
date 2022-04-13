0ecbea0049c6e914d1ea77193476b062932d3ec8
Yü Li Chang <andi_lin@icloud.com>
2022-04-13 13:43:15 +0200

    added OKKK LEETS GOOOO

--- core/src/se2/groupb/monopoly/Monopoly.java
+++ core/src/se2/groupb/monopoly/Monopoly.java
@@ -1,18 +1,34 @@
 package se2.groupb.monopoly;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Audio;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+
+
+
 public class Monopoly extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	Music music;
+
+
+
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		music = Gdx.audio.newMusic(Gdx.files.internal("OkLetsGOOO.mp3"));
+		music.play();
+		music.setVolume(100);
+
 	}
 
 	@Override
@@ -21,6 +37,9 @@ public class Monopoly extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+
+
+
 	}
 	
 	@Override
@@ -28,4 +47,7 @@ public class Monopoly extends ApplicationAdapter {
 		batch.dispose();
 		img.dispose();
 	}
+
+
+
 }
