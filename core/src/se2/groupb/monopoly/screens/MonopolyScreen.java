16d5b58423fdbb6daee40190ec2ef422437032fa
Christian <christiandeutinger@hotmail.com>
2022-04-19 16:19:44 +0200

    Revert "changes for merging"
    This reverts commit d238ea94b632ee6259e2c4db34aa611e7af7cff2.

--- core/src/se2/groupb/monopoly/screens/MonopolyScreen.java
+++ core/src/se2/groupb/monopoly/screens/MonopolyScreen.java
@@ -15,7 +15,6 @@ public class MonopolyScreen implements Screen {
     Texture img;
     Music music;
     private Monopoly monopoly;
-    private BitmapFont menuFont;
 
     public MonopolyScreen(Monopoly monopoly) {
         this.monopoly = monopoly;
@@ -32,20 +31,13 @@ public class MonopolyScreen implements Screen {
     @Override
     public void render(float delta) {
         ScreenUtils.clear(1, 1, 1, 1);
-        int yPosInitialButtons = Gdx.graphics.getHeight() / 4;
-        int xPosButtons = Gdx.graphics.getWidth() / 2 - Gdx.graphics.getWidth() / 12;
-        // Start Game text on screen
-        menuFont = new BitmapFont();
-        menuFont.setColor(0, 0, 0, 1);
-        menuFont.getData().setScale(4, 4);
+
+
+
         monopoly.batch.begin();
-        // Tap screen to go to main menu
-        menuFont.draw(monopoly.batch, "START", xPosButtons, yPosInitialButtons);
-        if (Gdx.input.isButtonPressed(Input.Buttons.LEFT) || Gdx.input.isTouched()) {
-            // switch Screen
-            monopoly.setScreen(new MainMenuScreen(monopoly));
-        }
+
         monopoly.batch.draw(img, 0, 0);
+
         monopoly.batch.end();
     }
 
@@ -73,6 +65,5 @@ public class MonopolyScreen implements Screen {
     public void dispose() {
         img.dispose();
         music.dispose();
-        menuFont.dispose();
     }
 }
