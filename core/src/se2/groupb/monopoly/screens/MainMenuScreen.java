fd5f8645d1dd3d0d3ae50e1131adcc9ab37d02e7
Christian <christiandeutinger@hotmail.com>
2022-04-19 16:59:11 +0200

    update imports for merge

--- core/src/se2/groupb/monopoly/screens/MainMenuScreen.java
+++ core/src/se2/groupb/monopoly/screens/MainMenuScreen.java
@@ -1,6 +1,7 @@
 package se2.groupb.monopoly.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -21,6 +22,8 @@ public class MainMenuScreen implements Screen {
     private int yPosOffsetButtons;
     private int xPosButtons;
 
+    private int userPosX;
+    private int userPosY;
 
     /**
      * Constructor
@@ -34,19 +37,21 @@ public class MainMenuScreen implements Screen {
     public void show() {
         exitButton = new Texture("images/exit_button_inactive.png");
         playButton = new Texture("images/play_button_inactive.png");
-        joinButton = new Texture("images/play_button_inactive.png");
+        joinButton = new Texture("images/play_button_active.png");
 
         buttonSizeX = Gdx.graphics.getWidth() / 10;
         buttonSizeY = Gdx.graphics.getHeight() / 10;
 
-        yPosInitialButtons = Gdx.graphics.getHeight() / 4;
-        xPosButtons = Gdx.graphics.getWidth() / 2 - Gdx.graphics.getWidth() / 12;
+        xPosButtons = Gdx.graphics.getWidth() / 2 - buttonSizeX / 2;
         yPosInitialButtons = Gdx.graphics.getHeight() - Gdx.graphics.getHeight() / 4;
         yPosOffsetButtons = -Gdx.graphics.getWidth() / 8;
     }
 
     @Override
     public void render(float delta) {
+        userPosX = Gdx.input.getX();
+        userPosY = Gdx.graphics.getHeight() - Gdx.input.getY();
+
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         monopoly.batch.begin();
@@ -57,9 +62,41 @@ public class MainMenuScreen implements Screen {
         // Join Game Button
         monopoly.batch.draw(joinButton, xPosButtons, yPosInitialButtons + yPosOffsetButtons, buttonSizeX, buttonSizeY);
 
-
         // Exit game Button
-        monopoly.batch.draw(exitButton, xPosButtons, yPosInitialButtons + 2*yPosOffsetButtons, buttonSizeX, buttonSizeY);
+        monopoly.batch.draw(exitButton, xPosButtons, yPosInitialButtons + 2f * yPosOffsetButtons, buttonSizeX, buttonSizeY);
+
+        // Pressing the buttons leads to different screens
+        // borders of host button
+        if (userPosX > xPosButtons && userPosX < xPosButtons + buttonSizeX && userPosY > yPosInitialButtons && userPosY < yPosInitialButtons + buttonSizeY) {
+            /**
+             * if area is touched go into game
+             * TODO:
+             * make screen for hosting a game and let players go into online multiplayer
+             */
+            if (Gdx.input.isTouched() || Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+                this.dispose();
+                monopoly.setScreen(new MonopolyScreen(monopoly));
+            }
+        }
+
+        //borders of join button
+        if (userPosX > xPosButtons && userPosX < xPosButtons + buttonSizeX && userPosY > yPosInitialButtons + yPosOffsetButtons && userPosY < yPosInitialButtons + yPosOffsetButtons + buttonSizeY) {
+            /**
+             * TODO:
+             * make screen for joining a game via code
+             */
+            if (Gdx.input.isTouched() || Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+                monopoly.batch.draw(new Texture("images/badlogic.jpg"), xPosButtons, yPosInitialButtons + yPosOffsetButtons);
+            }
+        }
+
+        //borders of exit button if clicked then exit app
+        if (userPosX > xPosButtons && userPosX < xPosButtons + buttonSizeX && userPosY > yPosInitialButtons + 2 * yPosOffsetButtons && userPosY < yPosInitialButtons + 2 * yPosOffsetButtons + buttonSizeY) {
+            if (Gdx.input.isTouched() || Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+                this.dispose();
+                Gdx.app.exit();
+            }
+        }
 
         monopoly.batch.end();
     }
@@ -86,6 +123,8 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        exitButton.dispose();
+        playButton.dispose();
+        joinButton.dispose();
     }
 }
