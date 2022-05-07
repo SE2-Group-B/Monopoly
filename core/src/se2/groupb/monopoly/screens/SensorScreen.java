166899bb47b5ddf290ec857ef6837e1c40e2564b
MrsicMarko <marko2605@gmx.at>
2022-05-07 17:23:21 +0200

    Ansatz zum Würfeln, pach wenn Lautsprechertaste beim Handy gedrückt wird

--- core/src/se2/groupb/monopoly/screens/SensorScreen.java
+++ core/src/se2/groupb/monopoly/screens/SensorScreen.java
@@ -6,12 +6,15 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.utils.Logger;
 import com.badlogic.gdx.utils.Timer;
+
+import java.util.Random;
 
 import javax.swing.text.View;
 
@@ -22,9 +25,9 @@ import sun.rmi.runtime.Log;
 
 public class SensorScreen implements Screen {
 
-    Monopoly monopoly;
-    boolean gyroSensorActive=Gdx.input.isPeripheralAvailable(Input.Peripheral.Gyroscope);
-    boolean lightSensorActive = Gdx.input.isPeripheralAvailable(Input.Peripheral.Accelerometer);
+    private Monopoly monopoly;
+    private boolean gyroSensorActive=Gdx.input.isPeripheralAvailable(Input.Peripheral.Gyroscope);
+    private boolean lightSensorActive = Gdx.input.isPeripheralAvailable(Input.Peripheral.Accelerometer);
     private float xGyro;
     private float yGyro;
     private float zGyro;
@@ -32,6 +35,17 @@ public class SensorScreen implements Screen {
     private float yLight;
     private float zLight;
     private int toggleVibration;
+    private Texture rollDice;
+    private boolean cheatActivated;
+    private boolean onTurn;
+    private Random random;
+
+    private int buttonSizeX;
+    private int buttonSizeY;
+    private float yPosInitialButtons;
+    private float yPosOffsetButtons;
+    private float xPosButtons;
+
 
     public SensorScreen(Monopoly monopoly) {
         this.monopoly = monopoly;
@@ -45,6 +59,25 @@ public class SensorScreen implements Screen {
 
     @Override
     public void show() {
+
+        InputBackProcessor inputProcessor = new InputBackProcessor(monopoly);
+        inputProcessor.backToMainMenuProcessor();
+
+        rollDice = new Texture("images/play_button_inactive.png");
+        cheatActivated = false;
+        onTurn = true;
+
+        random = new Random();
+
+        buttonSizeX = Gdx.graphics.getWidth() / 3;
+        buttonSizeY = (int) (Gdx.graphics.getHeight() / (4.545454 * 2));
+
+        xPosButtons = (float) (Gdx.graphics.getWidth() / 2D - buttonSizeX / 2D);
+        yPosInitialButtons = (float) (Gdx.graphics.getHeight() - Gdx.graphics.getHeight() / 4D);
+        yPosOffsetButtons = (float) (-Gdx.graphics.getWidth() / 8D);
+
+
+
         stage = new Stage();
         toggleVibration = -1;
         Gdx.app.setLogLevel(Application.LOG_INFO);
@@ -75,15 +108,48 @@ public class SensorScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        float userPosX = (float) Gdx.input.getX();
+        float userPosY = (float) Gdx.graphics.getHeight() - Gdx.input.getY();
+        float firstDice = 0;
+        float secondDice = 0;
         monopoly.batch.begin();
-        if (Gdx.input.isKeyPressed(Input.Keys.VOLUME_DOWN)) {
-            toggleVibration=toggleVibration*-1;
+
+        //Roll Dice Button
+        monopoly.batch.draw(rollDice, xPosButtons, yPosInitialButtons, buttonSizeX, buttonSizeY);
+
+        /**
+         * Pressing the Roll Dice Button
+         */
+        if (isCorrectPosition(userPosX, userPosY, xPosButtons, yPosInitialButtons, buttonSizeX, buttonSizeY, 0 * yPosOffsetButtons)
+                && Gdx.input.isTouched() && onTurn) {
+
+            firstDice = random.nextInt(6)+1;
+            if (Gdx.input.isKeyPressed(Input.Keys.VOLUME_DOWN)) {
+                secondDice = firstDice;
+                cheatActivated = true;
+            }else {
+                secondDice = random.nextInt(6)+1;
+                cheatActivated = false;
+            }
+            Gdx.app.log("firstDice: ", String.valueOf(firstDice));
+            Gdx.app.log("secondDice: ", String.valueOf(secondDice));
+            onTurn = false;
+
+            if (firstDice == secondDice) {
+                onTurn = true;
+            }
         }
-        if(toggleVibration > 0){
-            Gdx.input.vibrate(100);
-        }
+
+//        if (Gdx.input.isKeyPressed(Input.Keys.VOLUME_DOWN)) {
+//            toggleVibration=toggleVibration*-1;
+//        }
+//        if(toggleVibration > 0){
+//            Gdx.input.vibrate(100);
+//        }
         monopoly.batch.end();
     }
+
+
 
     @Override
     public void resize(int width, int height) {
@@ -108,5 +174,9 @@ public class SensorScreen implements Screen {
     @Override
     public void dispose() {
 
+    }
+
+    private static boolean isCorrectPosition(float userPosX, float userPosY, float xPosButton, float yPosButton, float buttonSizeX, float buttonSizeY, float yPosOffset) {
+        return (userPosX > xPosButton && userPosX < xPosButton + buttonSizeX && userPosY > (yPosButton + yPosOffset) && userPosY < yPosButton + yPosOffset + buttonSizeY);
     }
 }
