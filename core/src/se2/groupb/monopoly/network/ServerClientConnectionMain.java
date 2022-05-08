6f9fa30defacf679620c4736e356713104992904
Christian <christiandeutinger@hotmail.com>
2022-05-08 10:41:10 +0200

    create new buttons for other team members

--- core/src/se2/groupb/monopoly/network/ServerClientConnectionMain.java
+++ core/src/se2/groupb/monopoly/network/ServerClientConnectionMain.java
@@ -5,11 +5,9 @@ package se2.groupb.monopoly.network;
  */
 public class ServerClientConnectionMain {
     public static void main(String[] args) {
-        ServerFoundation instance = new ServerFoundation();
-        ClientFoundation clients = new ClientFoundation();
-        ClientFoundation client2 = new ClientFoundation();
+        ServerFoundation instance = new ServerFoundation(6333,6333);
+        ClientFoundation clients = new ClientFoundation(6333,6333);
+        ClientFoundation client2 = new ClientFoundation(6333,6333);
         clients.getClient().sendUDP("Hallo ich bin ein Client");
-        clients.getClient().close();
-        instance.getServer().close();
     }
 }
