4608e4808e4ddd6049d41a468178753910433a4f
vivi0815 <vihaferkamp@edu.aau.at>
2022-06-01 13:28:17 +0200

    PropertyTest Coverage verbessern

--- core/test/propertyTest.java
+++ core/test/propertyTest.java
@@ -1,28 +1,49 @@
 //package test;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import se2.groupb.monopoly.Property;
 
 public class propertyTest {
 
+    Property g;
+
+    @Before
+    public void init(){
+        g=new Property("Grundstück");
+    }
+    @After
+    public void  teardown (){
+        g=null;
+    }
+
     @Test
     public void testGrundstueck(){
-        Property g=new Property("Grundstück");
-        Assert.assertNotNull(g);
+        Property g1=new Property("Grundstück");
+        Assert.assertNotNull(g1);
     }
     @Test
     public void testGetName(){
-        Property g=new Property("Grundstück");
         Assert.assertEquals(g.getName(), "Grundstück");
     }
 
     @Test
     public void testSetName(){
-        Property g=new Property("Grundstück");
         g.setName("Neues Grundstück");
         Assert.assertEquals(g.getName(), "Neues Grundstück");
+    }
+    @Test
+    public void testGetOwnerID(){
+        Assert.assertEquals(g.getOwnerId(), 0);
+    }
+
+    @Test
+    public void testSetOwnerID(){
+        g.setOwnerId(2);
+        Assert.assertEquals(g.getOwnerId(), 2);
     }
 
 }
