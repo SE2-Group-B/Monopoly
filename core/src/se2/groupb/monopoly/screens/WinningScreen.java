18781e8944c8612d759433cab75be12398426f52
alhrnjic <alenhrnjic8@gmail.com>
2022-06-02 14:14:18 +0200

    Winning Screen and Methods
    Prettier Winning Screen also first Buttons

--- core/src/se2/groupb/monopoly/screens/WinningScreen.java
+++ core/src/se2/groupb/monopoly/screens/WinningScreen.java
@@ -1,11 +1,15 @@
 package se2.groupb.monopoly.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 import se2.groupb.monopoly.CreateGameField;
 import se2.groupb.monopoly.Monopoly;
@@ -14,21 +18,28 @@ public class WinningScreen extends GameScreenAdapter{
 
     private BitmapFont font;
     private GlyphLayout waitingText;
+    private Environment environment;
 
 
     CreateGameField createGameField = new CreateGameField();
 
     SpriteBatch spriteBatch;
 
-    private String text = "Hallo";
-    private String first = "Sieger: " + createGameField.placement[0] + " with:" + createGameField.sums[0];
+    private String text = "Und die Sieger sind:";
+    /*private String first = "Sieger: " + createGameField.placement[0] + " with:" + createGameField.sums[0];
     private String second = "Zweiter: " + createGameField.placement[1] + " with:" + createGameField.sums[1];
     private String third = "Dritter: " + createGameField.placement[2] + " with:" + createGameField.sums[2];
-    private String fourth = "Vierter: " + createGameField.placement[3] + " with:" + createGameField.sums[3];
+    private String fourth = "Vierter: " + createGameField.placement[3] + " with:" + createGameField.sums[3];*/
+    private String first = "Sieger: Player 1 with: 2000$";
+    private String second = "Zweiter: Player 2 with: 1800$";
+    private String third = "Dritter: Player 3 with: 1600$";
+    private String fourth = "Vierter: Player 4 with: 100$";
 
 
     public WinningScreen(Monopoly monopoly) {
         super(monopoly);
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 1f, 1f, 1f, 1f));
     }
 
     @Override
@@ -41,12 +52,13 @@ public class WinningScreen extends GameScreenAdapter{
 
         font = new BitmapFont();
         spriteBatch = new SpriteBatch();
-        font.getData().setScale(3.5f);
+        font.getData().setScale(7f);
         waitingText = new GlyphLayout(font, text);
     }
 
     @Override
     public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0, 1);
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         float userPosX = (float) Gdx.input.getX();
@@ -54,13 +66,12 @@ public class WinningScreen extends GameScreenAdapter{
         monopoly.batch.begin();
         spriteBatch.begin();
 
-        font.draw(spriteBatch, text, Gdx.graphics.getHeight()/2,Gdx.graphics.getWidth()/2);
-        font.draw(spriteBatch, text, Gdx.graphics.getHeight()/2,Gdx.graphics.getWidth()+50);
-        font.draw(spriteBatch, text, Gdx.graphics.getHeight()/2,Gdx.graphics.getWidth()+75);
-        /*font.draw(spriteBatch, first, Gdx.graphics.getHeight()/2,Gdx.graphics.getWidth()+50);
-        font.draw(spriteBatch, second, Gdx.graphics.getHeight()/2,Gdx.graphics.getWidth()+75);
-        font.draw(spriteBatch, third, Gdx.graphics.getHeight()/2,Gdx.graphics.getWidth()+100);
-        font.draw(spriteBatch, fourth, Gdx.graphics.getHeight()/2,Gdx.graphics.getWidth()+125);*/
+        font.setColor(Color.BLACK);
+        font.draw(spriteBatch, text, (float) (Gdx.graphics.getWidth()/3.75),(Gdx.graphics.getHeight()/2)+250);
+        font.draw(spriteBatch, first, (float) (Gdx.graphics.getWidth()/3.75),(Gdx.graphics.getHeight()/2)+150);
+        font.draw(spriteBatch, second, (float) (Gdx.graphics.getWidth()/3.75), (Gdx.graphics.getHeight()/2)+50);
+        font.draw(spriteBatch, third, (float) (Gdx.graphics.getWidth()/3.75),(Gdx.graphics.getHeight()/2)-50);
+        font.draw(spriteBatch, fourth, (float) (Gdx.graphics.getWidth()/3.75), (Gdx.graphics.getHeight()/2)-150);
 
         spriteBatch.end();
         monopoly.batch.end();
@@ -88,7 +99,7 @@ public class WinningScreen extends GameScreenAdapter{
 
     @Override
     public void dispose() {
-
+        spriteBatch.dispose();
     }
 
     @Override
