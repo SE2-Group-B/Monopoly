aedffe5ce93728a20a11251e914f65446ffb51a1
Christian <christiandeutinger@hotmail.com>
2022-06-02 14:28:06 +0200

    setup for later (win resets game)

--- core/src/se2/groupb/monopoly/Monopoly.java
+++ core/src/se2/groupb/monopoly/Monopoly.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import se2.groupb.monopoly.network.ClientFoundation;
+import se2.groupb.monopoly.network.ServerFoundation;
 import se2.groupb.monopoly.screens.MainMenuScreen;
 import se2.groupb.monopoly.screens.SensorScreen;
 
@@ -15,6 +16,15 @@ public class Monopoly extends Game {
     SensorScreen sensors;
     int[] diceRoll;
     private ClientFoundation client;
+
+    /**
+     * for later, when online game can be finished
+     * do: add server from host
+     * when player won close clients and server and return to main menu
+     * addServer(ServerFoundation)
+     * closeServer()
+     */
+    /*private ServerFoundation server;*/
 
 
 
