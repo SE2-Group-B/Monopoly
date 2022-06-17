84de250f9952cfd387e0105f02c8d94cfe28351c
MrsicMarko <marko2605@gmx.at>
2022-06-17 18:38:31 +0200

    finish PlayerOperation

--- core/src/se2/groupb/monopoly/CreateGameField.java
+++ core/src/se2/groupb/monopoly/CreateGameField.java
@@ -275,7 +275,7 @@ public class CreateGameField extends GameScreenAdapter {
             public boolean handle(Event event) {
                 if(Gdx.input.justTouched()){
                    //winning();
-                    screenOutput = playerOperation.buying();
+                    buying();
                 }return true;
             }
         });
@@ -435,7 +435,7 @@ public class CreateGameField extends GameScreenAdapter {
             spriteBatch.draw(d2, xPosButtons, yPosInitialButtons - 400, 500, 500);
         }
 
-//
+
 //        private String screenOutputCheck () {
 //            String playerName = getCurrentPlayer().getName();
 //            String playerPosition = gameField.getGameField()[getCurrentPlayer().getPosition()].getName();
@@ -451,7 +451,7 @@ public class CreateGameField extends GameScreenAdapter {
 //                getCurrentPlayer().setPrison(true);
 //            }
 //        }
-//
+
         public static boolean isCorrectPosition(float userPosX, float userPosY, float xPosButton,
                                                 float yPosButton, float buttonSizeX, float buttonSizeY, float yPosOffset){
             return (userPosX > xPosButton && userPosX < xPosButton + buttonSizeX && userPosY > (yPosButton + yPosOffset) && userPosY < yPosButton + yPosOffset + buttonSizeY);
@@ -564,67 +564,44 @@ public class CreateGameField extends GameScreenAdapter {
             monopoly.setScreen(new WinningScreen(monopoly, sum, placement));
         }
 
-//        public void buying() {
-//            int pos = playerOperation.getCurrentPlayer().getPosition();
-//            if (!playerOperation.isSomeonesProperty(pos)) {
-//                String propertyType = getPropertyType(pos);
-//                switch (propertyType) {
-//                    case "Street":
-//                        Street s = (Street) gameField.getGameField()[pos];
-//                        playerOperation.getCurrentPlayer().changeMoney(-s.getPrice());
-//                        gameField.getGameField()[pos].setOwnerId(playerOperation.getCurrentPlayer().getId());
-//                        break;
-//                    case "Trainstation":
-//                        Trainstation t = (Trainstation) gameField.getGameField()[pos];
-//                        playerOperation.getCurrentPlayer().changeMoney(-t.getPrice());
-//                        gameField.getGameField()[pos].setOwnerId(playerOperation.getCurrentPlayer().getId());
-//                        playerOperation.getCurrentPlayer().setNumOfTrainstaitions(playerOperation.getCurrentPlayer().getNumOfTrainstaitions()+1);
-//                        //t.increaseRent();
-//                        break;
-//                    default:
-//                        if (gameField.getGameField()[pos].getOwnerId() == playerOperation.getCurrentPlayer().getId()) {
-//                            if (propertyType.equals("Street")) {
-//                                Street s1 = (Street) gameField.getGameField()[pos];
-//                                boolean bought = s1.buyhouse();
-//                                if (bought) {
-//                                    playerOperation.getCurrentPlayer().changeMoney(-s1.getHousePrice());
-//                                } else {
-//                                    break;
-//                                }
-//                            } else {
-//                                screenOutput = "Hier kannst du nichts zukaufen";
-//                            }
-//                        } else {
-//                            screenOutput = "Du kannst das nicht kaufen. Es gehört schon jemandem";
-//                        }
-//                }
-//            } else {
-//                screenOutput = "Du kannst das nicht kaufen. Es gehört schon jemandem";
-//            }
-//        }
-
-//        public String buying() {
-//            int playerPosition = playerOperation.getCurrentPlayer().getPosition();
-//            Property p = gameField.getGameField()[playerPosition];
-//            String output = "Player " + playerOperation.getCurrentPlayer().getName();
-//            if (!playerOperation.isSomeonesProperty(playerPosition)) {
-//                if (p instanceof Street) {
-//                    gameField.getGameField()[playerPosition].setOwnerId(playerOperation.getCurrentPlayer().getId());
-//                    output += " bought " + p.getName() + " for " + ((Street) p).getPrice() + "€";
-//                    playerOperation.getCurrentPlayer().changeMoney(-((Street) p).getPrice());
-//                } else if (p instanceof Trainstation) {
-//                    gameField.getGameField()[playerPosition].setOwnerId(playerOperation.getCurrentPlayer().getId());
-//                    playerOperation.getCurrentPlayer().setNumOfTrainstaitions(playerOperation.getCurrentPlayer().getNumOfTrainstaitions()+1);
-//                    output += " bought " + p.getName() + " for " + ((Trainstation) p).getPrice() + "€";;
-//                    playerOperation.getCurrentPlayer().changeMoney(-((Trainstation) p).getPrice());
-//                } else {
-//                    output = "You can't buy this Property";
-//                }
-//            }else {
-//                output = "You can't buy this Property";
-//            }
-//            return output;
-//        }
+        public void buying() {
+            int pos = playerOperation.getCurrentPlayer().getPosition();
+            if (!playerOperation.isSomeonesProperty(pos)) {
+                String propertyType = getPropertyType(pos);
+                switch (propertyType) {
+                    case "Street":
+                        Street s = (Street) gameField.getGameField()[pos];
+                        playerOperation.getCurrentPlayer().changeMoney(-s.getPrice());
+                        gameField.getGameField()[pos].setOwnerId(playerOperation.getCurrentPlayer().getId());
+                        break;
+                    case "Trainstation":
+                        Trainstation t = (Trainstation) gameField.getGameField()[pos];
+                        playerOperation.getCurrentPlayer().changeMoney(-t.getPrice());
+                        gameField.getGameField()[pos].setOwnerId(playerOperation.getCurrentPlayer().getId());
+                        playerOperation.getCurrentPlayer().setNumOfTrainstaitions(playerOperation.getCurrentPlayer().getNumOfTrainstaitions()+1);
+                        //t.increaseRent();
+                        break;
+                    default:
+                        if (gameField.getGameField()[pos].getOwnerId() == playerOperation.getCurrentPlayer().getId()) {
+                            if (propertyType.equals("Street")) {
+                                Street s1 = (Street) gameField.getGameField()[pos];
+                                boolean bought = s1.buyhouse();
+                                if (bought) {
+                                    playerOperation.getCurrentPlayer().changeMoney(-s1.getHousePrice());
+                                } else {
+                                    break;
+                                }
+                            } else {
+                                screenOutput = "Hier kannst du nichts zukaufen";
+                            }
+                        } else {
+                            screenOutput = "Du kannst das nicht kaufen. Es gehört schon jemandem";
+                        }
+                }
+            } else {
+                screenOutput = "Du kannst das nicht kaufen. Es gehört schon jemandem";
+            }
+        }
 
         private ArrayList<Player> initPlayerList(){
             players.add(player1);
