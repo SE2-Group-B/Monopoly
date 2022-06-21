e7e08ad3d6b002ec061918ca805c3f111077e1c3
Yuelichang <40342792+Yuelichang@users.noreply.github.com>
2022-06-21 13:38:48 +0200

    changing colors to the players color if the player buys the field - DONE

--- core/src/se2/groupb/monopoly/screens/MonopolyScreen.java
+++ core/src/se2/groupb/monopoly/screens/MonopolyScreen.java
@@ -153,7 +153,7 @@ public class MonopolyScreen extends GameScreenAdapter {
                     playerOperation.setMoneyPotForOperation(moneyPot);
                     screenOutput = playerOperation.checkCurrentProperty(playerOperation.getCurrentPlayer());
                     playerOperation.getCurrentPlayer().move(gameField.positions[playerOperation.getCurrentPlayer().getPosition()]);
-                    gameField.checkIfPlayerIsAlone(playerOperation.getCurrentPlayer());
+//                    gameField.checkIfPlayerIsAlone(playerOperation.getCurrentPlayer());
                 }
                 return true;
             }
