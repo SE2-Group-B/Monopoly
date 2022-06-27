632038b95d7db92d42c8b1c67c8aa8c9204b2801
MrsicMarko <marko2605@gmx.at>
2022-06-27 22:22:21 +0200

    add Tests to Pot and PlayerOperation

--- core/test/PlayerOPTest.java
+++ core/test/PlayerOPTest.java
@@ -7,73 +7,196 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
-import se2.groupb.monopoly.LogicalGameField;
 import se2.groupb.monopoly.Player;
 import se2.groupb.monopoly.PlayerOperation;
+import se2.groupb.monopoly.Pot;
 
 public class PlayerOPTest {
 
-    Player rot, blue;
-    ArrayList<Player> playerList = new ArrayList<>(2);
+    Player red;
+    Player blue;
+    ArrayList<Player> playerList;
     PlayerOperation playerOperation;
+    Pot pot;
 
     @Before
-    public void init(){
-        rot = new Player(1, "Rot", 2000,new ArrayList<>(),1 ,Color.RED);
-        blue = new Player(2, "Blue", 2000,new ArrayList<>(),1 ,Color.BLUE);
-        playerList.add(rot);
+    public void init() {
+        playerList = new ArrayList<>();
+        red = new Player(1, "Red", 2000, new ArrayList<>(), 0, Color.RED);
+        blue = new Player(2, "Blue", 2000, new ArrayList<>(), 1, Color.BLUE);
+        playerList.add(red);
         playerList.add(blue);
         playerOperation = new PlayerOperation(playerList);
+        pot = new Pot();
     }
+
     @After
-    public void  teardown (){
-
-    }
-
-
-    @Test
-    public void currentPropertyNormal(){
-        String something = playerOperation.checkCurrentProperty(rot);
-        Assert.assertEquals("Player Rot landed directly on GO and earned 400$", something);
+    public void teardown() {
+        red = null;
+        blue = null;
+        playerList = null;
+        playerOperation = null;
+        pot = null;
     }
 
     @Test
-    public void currentPropertyPrison(){
-        rot.setPosition(30);
-        String something = playerOperation.checkCurrentProperty(rot);
-        Assert.assertEquals("Player Rot went to prison", something);
+    public void testIsSomeonesPropertyFalse(){
+        Assert.assertFalse(playerOperation.isSomeonesProperty(5));
     }
 
     @Test
-    public void currentPropertyPrisonvisit(){
-        rot.setPosition(10);
-        String something = playerOperation.checkCurrentProperty(rot);
-        Assert.assertEquals("Player Rot is just visiting the prison", something);
+    public void testIsSomeonesPropertyTrue(){
+        red.setPosition(5);
+        playerOperation.buying();
+        Assert.assertTrue(playerOperation.isSomeonesProperty(5));
+    }
+
+    @Test
+    public void testCheckSolePropertyLosEarnMoney(){
+        red.move(40);
+        playerOperation.checkCurrentProperty(red);
+        Assert.assertEquals(2400, red.getBankBalance());
+    }
+
+    @Test
+    public void testCheckSolePropertySofa(){
+        pot.addToPot(200);
+        red.move(20);
+        playerOperation.setMoneyPotForOperation(pot);
+        playerOperation.checkCurrentProperty(red);
+        Assert.assertEquals(2200, red.getBankBalance());
+        Assert.assertEquals(0, pot.getAmount());
+    }
+
+    @Test
+    public void testCheckSolePropertyGoToJail(){
+        red.move(30);
+        playerOperation.checkCurrentProperty(red);
+        Assert.assertTrue(red.getPrison());
+        Assert.assertEquals(10, red.getPosition());
+    }
+
+    @Test
+    public void testCheckSolePropertyVisitPrison(){
+        red.move(10);
+        String output = playerOperation.checkCurrentProperty(red);
+        Assert.assertFalse(red.getPrison());
+        Assert.assertEquals("Player Red is just visiting the prison", output);
+    }
+
+    @Test
+    public void testCannotBuyPenaltyField(){
+        red.move(4);
+        playerOperation.buying();
+        Assert.assertFalse(playerOperation.isSomeonesProperty(4));
+    }
+
+    @Test
+    public void testNextPlayer(){
+        playerOperation.nextPlayer();
+        Assert.assertEquals(2, playerOperation.getCurrentPlayer().getId());
+    }
+
+    @Test
+    public void testNextPlayerFalse(){
+        playerOperation.nextPlayer();
+        Assert.assertNotEquals(1, playerOperation.getCurrentPlayer().getId());
+    }
+
+    @Test
+    public void testNextPlayerReset(){
+        playerOperation.nextPlayer();
+        playerOperation.nextPlayer();
+        Assert.assertEquals(1, playerOperation.getCurrentPlayer().getId());
+    }
+
+    @Test
+    public void testIsEnemyPropertyFalse(){
+        red.move(5);
+        Assert.assertFalse(playerOperation.isEnemyProperty(red.getPosition()));
+    }
+
+    @Test
+    public void testIsEnemyPropertyTrue(){
+        red.move(5);
+        playerOperation.buying();
+        playerOperation.nextPlayer();
+        blue.move(5);
+        Assert.assertTrue(playerOperation.isEnemyProperty(blue.getPosition()));
+    }
+
+    @Test
+    public void testGetCurrentPlayer(){
+        Assert.assertEquals(red, playerOperation.getCurrentPlayer());
+        playerOperation.nextPlayer();
+        Assert.assertEquals(blue, playerOperation.getCurrentPlayer());
+    }
+
+    @Test
+    public void testGetPlayerById(){
+        Assert.assertEquals(red, playerOperation.getPlayerById(1));
+        Assert.assertEquals(blue, playerOperation.getPlayerById(2));
+    }
+
+    @Test
+    public void testGetPropertyOwner(){
+        red.move(5);
+        playerOperation.buying();
+        Assert.assertEquals(red, playerOperation.getPropertyOwner(5));
+    }
+
+    @Test
+    public void testCheckCurrentPropertyPenalty(){
+        red.move(4);
+        playerOperation.setMoneyPotForOperation(pot);
+        playerOperation.checkCurrentProperty(red);
+        Assert.assertEquals(200, pot.getAmount());
+        Assert.assertEquals(1800, red.getBankBalance());
+    }
+
+    @Test
+    public void currentPropertyNormal() {
+        String something = playerOperation.checkCurrentProperty(red);
+        Assert.assertEquals("Player Red landed directly on GO and earned 400$", something);
+    }
+
+    @Test
+    public void currentPropertyPrison() {
+        red.setPosition(30);
+        String something = playerOperation.checkCurrentProperty(red);
+        Assert.assertEquals("Player Red went to prison", something);
+    }
+
+    @Test
+    public void currentPropertyPrisonvisit() {
+        red.setPosition(10);
+        String something = playerOperation.checkCurrentProperty(red);
+        Assert.assertEquals("Player Red is just visiting the prison", something);
     }
 
     @Test
     public void nextPlayer() {
         String something = playerOperation.nextPlayer();
-        Assert.assertEquals("It's Blue's turn now",something);
+        Assert.assertEquals("It's Blue's turn now", something);
     }
 
     @Test
-    public void buyingStreet(){
-        rot.setPosition(1);
+    public void buyingStreet() {
+        red.setPosition(1);
         String something = playerOperation.buying();
-        Assert.assertEquals("Player Rot bought Badstraße for 40€", something);
+        Assert.assertEquals("Player Red bought Badstraße for 40€", something);
     }
 
     @Test
-    public void buyingTrain(){
-        rot.setPosition(25);
+    public void buyingTrain() {
+        red.setPosition(25);
         String something = playerOperation.buying();
-        Assert.assertEquals("Player Rot bought Nordbahnhof for 500€", something);
+        Assert.assertEquals("Player Red bought Nordbahnhof for 500€", something);
     }
 
     @Test
-    public void buyingStreetfromother(){
-        rot.setPosition(1);
+    public void buyingStreetfromother() {
+        red.setPosition(1);
         playerOperation.buying();
         playerOperation.nextPlayer();
         blue.setPosition(1);
@@ -82,32 +205,18 @@ public class PlayerOPTest {
     }
 
     @Test
-    public void buyingStreetwitout(){
-        rot.setPosition(1);
-        rot.setBankBalance(0);
+    public void buyingStreetwitout() {
+        red.setPosition(1);
+        red.setBankBalance(0);
         String something = playerOperation.buying();
-        Assert.assertEquals("Player Rot bought nothing because he is too poor for that building", something);
+        Assert.assertEquals("Player Red bought nothing because he is too poor for that building", something);
     }
 
     @Test
-    public void buyingtwice(){
-        rot.setPosition(1);
+    public void buyingtwice() {
+        red.setPosition(1);
         playerOperation.buying();
         String something = playerOperation.buying();
         Assert.assertEquals("Du hast was zugekauft", something);
     }
-
-    /*@Test
-    public void Ereignis(){
-        rot.setPosition(22);
-        String something = playerOperation.checkCurrentProperty(rot);
-        Assert.assertEquals("true", something);
-    }
-
-    @Test
-    public void Gemeinschaft(){
-        rot.setPosition(17);
-        String something = playerOperation.checkCurrentProperty(rot);
-        Assert.assertEquals("true", something);
-    }*/
 }
--- core/test/PotTest.java
+++ core/test/PotTest.java
@@ -14,27 +14,21 @@ import se2.groupb.monopoly.Property;
 public class PotTest {
 
     Pot myPot;
-    Player rot;
+    Player red;
     ArrayList<Property> myPropertiesRot;
 
     @Before
     public void init(){
         myPot=new Pot();
         myPropertiesRot=new ArrayList<>();
-        rot=new Player(1, "Rot", 2000, myPropertiesRot,1 , Color.RED);
+        red =new Player(1, "Red", 2000, myPropertiesRot,1 , Color.RED);
     }
 
     @After
     public void teardown(){
         myPot=null;
-        rot=null;
+        red =null;
         myPropertiesRot=null;
-    }
-
-    @Test
-    public void testPotKonstruktor(){
-        Pot testPot=new Pot();
-        Assert.assertNotNull(testPot);
     }
 
     @Test
@@ -49,39 +43,28 @@ public class PotTest {
     }
 
     @Test
-    public void testDonateToPotString(){
-        Assert.assertEquals("Rot donates 100€ to the Pot.", myPot.donateToPot(rot, 100));
-    }
-
-    @Test
     public void testDonateToPotAmount(){
-        myPot.donateToPot(rot, 200);
+        myPot.donateToPot(red, 200);
         Assert.assertEquals(200, myPot.getAmount());
     }
 
     @Test
     public void testDonateToPotPlayerBankBalance(){
-        myPot.donateToPot(rot, 200);
-        Assert.assertEquals(1800, rot.getBankBalance());
-    }
-
-    @Test
-    public void testWinPotString(){
-        myPot.addToPot(500);
-        Assert.assertEquals("Rot won the Pot with 500€.", myPot.winPot(rot));
+        myPot.donateToPot(red, 200);
+        Assert.assertEquals(1800, red.getBankBalance());
     }
 
     @Test
     public void testWinPotPlayerBankAccount(){
         myPot.addToPot(500);
-        myPot.winPot(rot);
-        Assert.assertEquals(2500, rot.getBankBalance());
+        myPot.winPot(red);
+        Assert.assertEquals(2500, red.getBankBalance());
     }
 
     @Test
     public void testWinPotEmpty(){
         myPot.addToPot(400);
-        myPot.winPot(rot);
+        myPot.winPot(red);
         Assert.assertEquals(0, myPot.getAmount());
     }
 }
